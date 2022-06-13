b09cd8d68a94301a90aac8beacb6c7318c7b7d91
ATARU210 <samuel.vergel@outlook.com>
2022-06-13 17:44:37 -0500

    header with add and images puta kousey

---
+++ R-PicsArts/src/app/Components/left-bar/left-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'PicsArts-left-bar',
+  templateUrl: './left-bar.component.html',
+  styleUrls: ['./left-bar.component.css']
+})
+export class LeftBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
