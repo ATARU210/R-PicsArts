3509537c73f25eb4a8d859ee18095797987d9d58
ATARU210 <samuel.vergel@outlook.com>
2022-06-13 17:27:36 -0500

    first branch with images header

--- R-PicsArts/src/app/app.module.ts
+++ R-PicsArts/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { LeftBarComponent } from './Components/left-bar/left-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftBarComponent
   ],
   imports: [
     BrowserModule
