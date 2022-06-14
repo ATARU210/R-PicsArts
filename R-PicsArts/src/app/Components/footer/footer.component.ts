c35e4b80e4e2e43006a1828a102226c5e2b8a472
Miguel Veloza <mavelozao@correo.udistrital.edu.com>
2022-06-14 11:28:18 -0500

    Added label for footer div

--- R-PicsArts/src/app/Components/footer/footer.component.ts
+++ R-PicsArts/src/app/Components/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'PicsArts-Footer',
+  selector: 'PicsArts-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
