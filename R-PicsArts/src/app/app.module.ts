a893166feca1ec42ca0998080f19599d0fb59310
ATARU210 <samuel.vergel@outlook.com>
2022-06-14 15:23:34 -0500

    finishing left-bar

--- R-PicsArts/src/app/app.module.ts
+++ R-PicsArts/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { LeftBarComponent } from './Components/left-bar/left-bar.component';
+import { HeaderPrincipalMenuComponent } from './Components/header-principal-menu/header-principal-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LeftBarComponent
+    LeftBarComponent,
+    HeaderPrincipalMenuComponent
   ],
   imports: [
     BrowserModule
